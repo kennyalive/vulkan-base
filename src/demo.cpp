51bef11b6ad9d4935c5b1c84ce1a98fdcb1f1588
Artem Kharytoniuk <artemalive@gmail.com>
2023-01-22 18:51:06 +0100

    Move imgui backend files

--- src/demo.cpp
+++ src/demo.cpp
@@ -2,9 +2,8 @@
 
 #include "glfw/glfw3.h"
 #include "imgui/imgui.h"
-#include "imgui/imgui_internal.h"
-#include "imgui/impl/imgui_impl_vulkan.h"
-#include "imgui/impl/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_vulkan.h"
+#include "imgui/imgui_impl_glfw.h"
 
 namespace {
 struct Uniform_Buffer {
